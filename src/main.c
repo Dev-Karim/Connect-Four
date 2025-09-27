fc92c2075bb6b102e7610528610c61c3383ad7bd
Joe Habre <slaizywtff.net@gmail.com>
2025-09-27 13:46:37 +0300

    Update main.c
    Edited by Joe Habre

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
-#include "board.h"
 #include "game.h"
-#include <stdio.h>
-#include <stdlib.h>
-/*
-command to run in terminal (for now):
-    gcc src/main.c src/board.c src/game.c -o connect4
-    .\connect4.exe
 
-*/
+int main(void) {
+    play_human_vs_human();
+    return 0;
+}
