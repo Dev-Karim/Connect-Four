90222236762ca306bc6d7bdd3c28d0d28482515a
Joe Habre <slaizywtff.net@gmail.com>
2025-10-18 10:42:51 +0300

    Update game.h

--- src/game.h
+++ src/game.h
@@ -1,7 +1,10 @@
 #ifndef GAME_H
 #define GAME_H
 
-// Sprint 1: Two-player Connect Four (console).
+// Two-player console game (Sprint 1)
 void play_human_vs_human(void);
+
+// Sprint 2: Human vs Bot with difficulty
+void play_human_vs_bot(int difficulty);
 
 #endif
