2734f580f376cae60133c223874d9a3832799142
Joe Habre <slaizywtff.net@gmail.com>
2025-11-19 08:25:27 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "game.h"
 
 int main(void) {
-    int mode = 0, diff = 1;
+    int mode = 0, diff = DIFF_EASY;
 
     puts("Connect Four");
     puts("1) Player vs Player");
@@ -10,17 +10,19 @@ int main(void) {
     printf("Choose mode (1-2): ");
     if (scanf("%d", &mode) != 1) return 0;
 
-    // eat leftover newline so game input works with fgets
     int ch; while ((ch = getchar()) != '\n' && ch != EOF) {}
 
     if (mode == 2) {
         puts("Difficulty:");
-        puts("  1) Easy  (random valid moves)");
-        puts("  2) Medium(win-if-possible, block, center, else random)");
-        printf("Choose difficulty (1-2): ");
-        if (scanf("%d", &diff) != 1) diff = 1;
+        puts("  1) Easy   (random valid moves)");
+        puts("  2) Medium (win/block + center)");
+        puts("  3) Hard   (minimax + alpha-beta)");
+        printf("Choose difficulty (1-3): ");
+        if (scanf("%d", &diff) != 1) diff = DIFF_EASY;
         while ((ch = getchar()) != '\n' && ch != EOF) {}
-        if (diff != 2) diff = 1;
+
+        if (diff < DIFF_EASY || diff > DIFF_HARD) diff = DIFF_EASY;
+
         play_human_vs_bot(diff);
     } else {
         play_human_vs_human();
