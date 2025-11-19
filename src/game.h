7562a51c4b9b0dfb2ead9554143032e0634a3945
Joe Habre <slaizywtff.net@gmail.com>
2025-11-19 08:23:59 +0200

    Add difficulty levels for bot gameplay

--- src/game.h
+++ src/game.h
@@ -1,10 +1,15 @@
 #ifndef GAME_H
 #define GAME_H
 
+// difficulty levels
+#define DIFF_EASY   1
+#define DIFF_MEDIUM 2
+#define DIFF_HARD   3
+
 // Sprint 1: human vs. human
 void play_human_vs_human(void);
 
-// Sprint 2: human vs. bot
+// Sprint 2+3+4: human vs. bot (all difficulties)
 void play_human_vs_bot(int difficulty);
 
 #endif
